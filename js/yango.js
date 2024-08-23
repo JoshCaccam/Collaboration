a21e765156fc67d047308cabab5b8cf3cb99180a
JoshCaccam <jjcaccam84@gmail.com>
2024-08-23 12:03:20 +0800

    Header + scss

---
+++ js/yango.js
@@ -0,0 +1,7 @@
+const navSpan = document.querySelector(".header_span");
+const navToggles = document.querySelector(".header_toggles");
+
+navSpan.addEventListener("click", () => {
+    navSpan.classList.toggle("open");
+    navToggles.classList.toggle("open");
+ });
